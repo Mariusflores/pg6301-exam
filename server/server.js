51b47dacf49ad7fa7b3acdd11077d83ba85d956b
Marius <mfloresdal@gmail.com>
2022-11-14 11:35:49 +0100

    basic react app with express server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express();
+
+app.use(express.static("../client/dist"))
+
+
+const server = app.listen(process.env.PORT || 3000, () => {
+
+    console.log("server started at http://localhost:" + server.address().port)
+
+})
