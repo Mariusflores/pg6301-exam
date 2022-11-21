bf035dea46f2fbfdf7f29df491feedccb551bf3a
Marius <mfloresdal@gmail.com>
2022-11-21 19:19:32 +0100

    commented out login code for deployment to azure

--- server/server.js
+++ server/server.js
@@ -13,12 +13,14 @@ const app = express();
 
 
 //Login
-
+/*
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
+ */
 
 //Database connection
 const mongoClient = new MongoClient(process.env.MONGODB_URL);
@@ -28,7 +30,7 @@ mongoClient.connect().then(async () => {
 });
 
 // login
-
+/*
 app.use((req, res, next) => {
     const {username} = req.signedCookies;
     req.user = users.find(u => u.username === username);
@@ -76,6 +78,8 @@ app.get("/users", (req, res) => {
     }
     res.json(users.map(({fullName, username}) => ({username, fullName}) ));
 })
+
+ */
 
 app.use(express.static("../client/dist"))
 
